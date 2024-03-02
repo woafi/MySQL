c1d577527537a7ba48bae518e82bd0a0969bd44a
Mohammad Woafi <woafisun@yahoo.com>
2024-03-02 09:38:28 +0600

    New File

--- Basic Systax.sql
+++ Basic Systax.sql
@@ -1,15 +1,22 @@
 CREATE DATABASE College;
 USE College;
+
+#CREATE TABLE
 CREATE TABLE Student(
 	Rollno INT PRIMARY KEY,
     Name VARCHAR(50),
     Registration INT UNIQUE,
 	Age INT NOT NULL CHECK (Age >=18)
     );
+
+#INSERT
 INSERT INTO Student (Rollno, Name, Registration, Age) VALUES (1, "AMAN", 28391274, 19), (2, "RAHIM", 28391279, 18);
 INSERT INTO Student VALUES (3, "WOAFI", 28391273, 19);
+
 SELECT * FROM Student;
+
 SHOW TABLES;
+
 DROP TABLE Student;
 
 
---
+++ DQL.sql
@@ -0,0 +1,47 @@
+CREATE DATABASE College;
+USE College;
+
+CREATE TABLE Student(
+	Rollno INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Marks INT NOT NULL,
+    Grade VARCHAR(1),
+    City VARCHAR(20)
+    );
+INSERT INTO Student 
+(Rollno, Name, Marks, Grade, City) 
+VALUES 
+(101, "AMAN", 78,"C","Pune"), 
+(102, "RAHIM", 96, "A", "Mumbai"),
+(103, "WOAFI", 85, "B", "Mumbai"),
+(104, "ROBI", 83, "A", "Delhi"),
+(105, "FARAH", 12, "F", "Delhi"),
+(106, "LUMINE", 82, "B", "Delhi");
+
+#SELECT
+SELECT * FROM Student;
+SELECT DISTINCT City FROM Student;
+
+#WHERE CLAUSE
+SELECT * FROM Student WHERE Marks > 80 AND City ="Mumbai";
+SELECT * FROM Student WHERE Marks > 80 OR City ="Mumbai";
+SELECT * FROM Student WHERE Marks+10 > 100;
+SELECT * FROM Student WHERE Marks BETWEEN 80 AND 90;
+SELECT * FROM Student WHERE City IN ("Delhi", "Mumbai");
+SELECT * FROM Student WHERE City NOT IN ("Delhi", "Mumbai");
+SELECT * FROM Student WHERE City IN ("Jhenaidah", "Dhaka");
+SELECT * FROM Student WHERE Marks > 75 LIMIT 3;
+
+#ORDER CLAUSE
+SELECT * FROM Student ORDER BY City DESC;
+
+#Aggregration function
+SELECT MAX(Marks) FROM Student; 
+SELECT MIN(Marks) FROM Student;
+SELECT AVG(Marks) FROM Student;
+SELECT COUNT(Name) FROM Student;
+
+#WHERE CLAUSE
+SELECT City, COUNT(Rollno) FROM Student GROUP BY City;
+
+DROP TABLE Student;
